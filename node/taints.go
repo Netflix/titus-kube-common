8396e01178a6e98329631d7219541eb6de70c260
Jigish Patel <jigishp@netflix.com>
2020-10-30 11:26:14 -0700

    rename evac taint to work around tolerationSeconds

--- node/taints.go
+++ node/taints.go
@@ -5,7 +5,7 @@ const (
 	TaintKeyFarzone      = "node.titus.netflix.com/farzone"
 	TaintKeyGPUNode      = "node.titus.netflix.com/gpu"
 	TaintKeyInit         = "node.titus.netflix.com/uninitialized"
-	TaintKeyNodeEvacuate = "taint.titus.netflix.com/nodeEvacuate"
+	TaintKeyNodeEvacuate = "taint.titus.netflix.com/evacuate"
 	TaintKeyNodeProblem  = "taint.titus.netflix.com/nodeProblem"
 	TaintKeyScheduler    = "node.titus.netflix.com/scheduler"
 	TaintKeyTier         = "node.titus.netflix.com/tier"
