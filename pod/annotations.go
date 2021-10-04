ade9bb5fd3cb1d7fd2aa8d893c5b34e5607a275d
Yun Wang <yunw@netflix.com>
2021-10-04 15:27:51 -0700

    Add annotation key for platform sidecar version

--- pod/annotations.go
+++ pod/annotations.go
@@ -166,6 +166,7 @@ const (
 	AnnotationKeyPrefixSidecarsLegacy                = "titusParameter.agent.sidecars"
 	AnnotationKeySuffixSidecarsChannelOverride       = "channel-override"
 	AnnotationKeySuffixSidecarsChannelOverrideReason = "channel-override-reason"
+	AnnotationKeySuffixSidecarsVersion               = "version"
 	AnnotationKeySidecarsIncludeLegacy               = AnnotationKeyPrefixSidecarsLegacy + "/include"
 )
 
