c0ec6362f04e66d24df694308f8fbc99034f8fa9
Igor Okulist <iokulist@netflix.com>
2022-08-03 16:06:12 -0500

    TITUS-6120 Extract common constants
    Add function for checking for mockPod

--- pod/annotations.go
+++ pod/annotations.go
@@ -208,7 +208,7 @@ const (
 	AnnotationKeyPredRuntimeModelID      = "runtime.predictions.titus.netflix.com/model-id"
 
 	// mockPod
-	AnnotationKeyPodParameterMockPodPrepareTime = "mockPod.netflix.com/killTime"
+	AnnotationKeyPodParameterMockPodPrepareTime = "mockPod.netflix.com/prepareTime"
 	AnnotationKeyPodParameterMockPodRunTime     = "mockPod.netflix.com/runTime"
 	AnnotationKeyPodParameterMockPodKillTime    = "mockPod.netflix.com/killTime"
 )
@@ -706,4 +706,9 @@ func PlatformSidecars(annotations map[string]string) ([]PlatformSidecar, error) 
 	}
 
 	return sidecars, nil
+}
+
+func IsMockPod(pod *corev1.Pod) bool {
+	_, ok := pod.Annotations[AnnotationKeyPodParameterMockPodRunTime]
+	return ok
 }
