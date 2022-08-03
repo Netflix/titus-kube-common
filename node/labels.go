77ceb34f3e51d1135dc7e2f443c65dd60caeefe2
Igor Okulist <iokulist@netflix.com>
2022-08-03 15:51:29 -0500

    TITUS-6120 Extract common constants
    Add node label value for mock backend

--- node/labels.go
+++ node/labels.go
@@ -1,5 +1,9 @@
 package node
 
+import (
+	corev1 "k8s.io/api/core/v1"
+)
+
 const (
 	LabelKeyASG             = "node.titus.netflix.com/asg"
 	LabelKeyBackend         = "node.titus.netflix.com/backend"
@@ -11,4 +15,13 @@ const (
 	LabelKeyInstanceType    = "node.kubernetes.io/instance-type"
 	LabelKeyMutableBuild    = "node.titus.netflix.com/mutable-build"
 	LabelKeyCpuModelName    = "node.titus.netflix.com/cpu-model-name"
+
+	LabelValueBackendMock = "mock"
 )
+
+func IsMockNode(node *corev1.Node) bool {
+	if val, ok := node.Labels[LabelKeyBackend]; ok {
+		return val == LabelValueBackendMock
+	}
+	return false
+}
