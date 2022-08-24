571cc2484ad8d7b6a034fef9b1ef07c08dd43cf6
Kyle Anderson <kylea@netflix.com>
2022-08-24 16:51:50 -0700

    Added more node labels

--- node/labels.go
+++ node/labels.go
@@ -5,18 +5,21 @@ import (
 )
 
 const (
-	LabelKeyASG             = "node.titus.netflix.com/asg"
-	LabelKeyBackend         = "node.titus.netflix.com/backend"
-	LabelKeyDecommissioning = "node.titus.netflix.com/decommissioning"
-	LabelKeyInstanceID      = "node.titus.netflix.com/id"
-	LabelKeyRemovable       = "node.titus.netflix.com/removable"
-	LabelKeyResourcePool    = "scaler.titus.netflix.com/resource-pool"
-	LabelKeyTerminating     = "node.titus.netflix.com/terminating"
-	LabelKeyInstanceType    = "node.kubernetes.io/instance-type"
-	LabelKeyMutableBuild    = "node.titus.netflix.com/mutable-build"
-	LabelKeyCpuModelName    = "node.titus.netflix.com/cpu-model-name"
+	LabelKeyASG               = "node.titus.netflix.com/asg"
+	LabelKeyBackend           = "node.titus.netflix.com/backend"
+	LabelKeyDecommissioning   = "node.titus.netflix.com/decommissioning"
+	LabelKeyInstanceID        = "node.titus.netflix.com/id"
+	LabelKeyRemovable         = "node.titus.netflix.com/removable"
+	LabelKeyResourcePool      = "scaler.titus.netflix.com/resource-pool"
+	LabelKeyServerGroupIgnore = "scaler.titus.netflix.com/ignore"
+	LabelKeyTerminating       = "node.titus.netflix.com/terminating"
+	LabelKeyInstanceType      = "node.kubernetes.io/instance-type"
+	LabelKeyMutableBuild      = "node.titus.netflix.com/mutable-build"
+	LabelKeyCpuModelName      = "node.titus.netflix.com/cpu-model-name"
 
-	LabelValueBackendMock = "mock"
+	LabelValueBackendMock           = "mock"
+	LabelValueBackendVirtualKubelet = "VirtualKubelet"
+	LabelValueBackendKubelet        = "kubelet"
 )
 
 func IsMockNode(node *corev1.Node) bool {
