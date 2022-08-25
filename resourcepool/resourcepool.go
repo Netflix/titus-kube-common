4c65fc2ff831fedd7a2d5d253ebfefd4eccee17c
Igor Okulist <iokulist@netflix.com>
2022-08-25 14:44:43 -0500

    Clean up resourcepool names

--- resourcepool/resourcepool.go
+++ resourcepool/resourcepool.go
@@ -1,12 +1,11 @@
 package resourcepool
 
 const (
-	ResourcePoolElastic    = "elastic"
-	ResourcePoolElasticGPU = "elasticGpu"
-	ResourcePoolReserved   = "reserved"
-	ResourcePoolMocNodes   = "mock-nodes"
+	ResourcePoolElastic   = "elastic"
+	ResourcePoolReserved  = "reserved"
+	ResourcePoolMockNodes = "mock-nodes"
 )
 
 func IsMockResourcePool(poolName string) bool {
-	return poolName == ResourcePoolMocNodes
+	return poolName == ResourcePoolMockNodes
 }
